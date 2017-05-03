9d87c421272f1ba68126d7b8df34dcb05a9ef68b
Beren İlkim Ceylan <berenilkim.ceylan@v-count.com>
2017-05-03 11:37:12 +0300

    readme.md file and credits

---
+++ README.md
@@ -0,0 +1,14 @@
+# Yet another selenium example to work with Chrome ##
+
+An example to show how selenium library works with chrome and how to add extra options such as,
+
+safe browsing,
+different download directory,
+disable download prompt
+
+Happy testing!
+
+
+## Requirements:
++ pip install selenium
+
--- seleniumChrome.py
+++ seleniumChrome.py
@@ -1,15 +1,33 @@
-# -- Selenium vars -- #
-chromedriver = "pat\\to\\your\\chromedriver.exe"
+import os
+from selenium import webdriver
+
+##########################
+##        Author        ##
+##  Beren İlkim Ceylan  ##
+## bceylanues@gmail.com ##
+##     @berenceylan     ##
+##########################
+
+
+# -- Chrome options -- #
+chromedriver = "path\\to\\your\\chromedriver.exe"
 os.environ["webdriver.chrome.driver"] = chromedriver
 options = webdriver.ChromeOptions()
 
 options.add_experimental_option("prefs", {
-  "download.default_directory": path,
+  "download.default_directory": "path\\to\\download",
+  "download.directory_upgrade": True,  
   "download.prompt_for_download": False,
-  "download.directory_upgrade": True,
   "safebrowsing.enabled": True 
  
 })
 options.add_argument("--start-maximized")
 
 driver = webdriver.Chrome(executable_path=chromedriver, chrome_options=options)
+driver.get("http://google.com")
+
+#Search bar
+driver.find_element_by_id("q").send_keys(username)
+
+#Click button
+driver.find_element_by_name("btnK").click()
